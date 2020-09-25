b610dbd1578f3016d3dad93686d0de46d6fd5a99
AymenSekhri <aymen.sekhri@live.fr>
2020-09-25 12:12:30 +0100

    Fixed few bugs.

--- minimalist_malloc/malloc.cpp
+++ minimalist_malloc/malloc.cpp
@@ -1,38 +1,41 @@
 #include "slab_allocator.h"
 
 const size_t PAGE_SIZE = 0x1000;
-static Slab<0x010, PAGE_SIZE> slab_0x10;
-static Slab<0x020, PAGE_SIZE> slab_0x20;
-static Slab<0x040, PAGE_SIZE> slab_0x40;
-static Slab<0x060, PAGE_SIZE> slab_0x60;
-static Slab<0x100, PAGE_SIZE> slab_0x100;
-static Slab<0x200, PAGE_SIZE> slab_0x200;
-static Slab<0x300, PAGE_SIZE> slab_0x300;
+alignas(PAGE_SIZE) static Slab<0x010, PAGE_SIZE> slab_0x10;
+alignas(PAGE_SIZE) static Slab<0x020, PAGE_SIZE> slab_0x20;
+alignas(PAGE_SIZE) static Slab<0x040, PAGE_SIZE> slab_0x40;
+alignas(PAGE_SIZE) static Slab<0x060, PAGE_SIZE> slab_0x80;
+alignas(PAGE_SIZE) static Slab<0x100, PAGE_SIZE> slab_0x100;
+alignas(PAGE_SIZE) static Slab<0x200, PAGE_SIZE> slab_0x200;
+alignas(PAGE_SIZE) static Slab<0x300, PAGE_SIZE> slab_0x300;
 
 void custom_malloc_init() {
 	slab_0x10.init();
 	slab_0x20.init();
 	slab_0x40.init();
-	slab_0x60.init();
+	slab_0x80.init();
 	slab_0x100.init();
 	slab_0x200.init();
 	slab_0x300.init();
 }
 
 void* custom_malloc(size_t size) {
-	if (size < 0x10) {
+	if (!size) {
+		return nullptr;
+	}
+	if (size <= 0x10) {
 		return slab_0x10.alloc();
-	} else if (size < 0x20) {
-		return slab_0x10.alloc();
-	} else if (size < 0x40) {
+	} else if (size <= 0x20) {
+		return slab_0x20.alloc();
+	} else if (size <= 0x40) {
 		return slab_0x40.alloc();
-	} else if (size < 0x60) {
-		return slab_0x60.alloc();
-	} else if (size < 0x100) {
+	} else if (size <= 0x80) {
+		return slab_0x80.alloc();
+	} else if (size <= 0x100) {
 		return slab_0x100.alloc();
-	} else if (size < 0x200) {
+	} else if (size <= 0x200) {
 		return slab_0x200.alloc();
-	} else if (size < 0x500) {
+	} else if (size <= 0x500) {
 		return slab_0x300.alloc();
 	} else {
 		return nullptr;
@@ -40,11 +43,25 @@ void* custom_malloc(size_t size) {
 }
 
 void custom_free(void* address) {
-	slab_0x10.free(address);
-	slab_0x20.free(address);
-	slab_0x40.free(address);
-	slab_0x60.free(address);
-	slab_0x100.free(address);
-	slab_0x200.free(address);
-	slab_0x300.free(address);
+	SlabHeader<1, PAGE_SIZE>* slab_header = reinterpret_cast<SlabHeader<1, PAGE_SIZE>*>(uintptr_t(address) & (~(PAGE_SIZE - 1)));
+	assert(slab_header->magic == MAGIC);
+
+	if (slab_header->size <= 0x10) {
+		return slab_0x10.free(address);
+	} else if (slab_header->size <= 0x20) {
+		return slab_0x20.free(address);
+	} else if (slab_header->size <= 0x40) {
+		return slab_0x40.free(address);
+	} else if (slab_header->size <= 0x80) {
+		return slab_0x80.free(address);
+	} else if (slab_header->size <= 0x100) {
+		return slab_0x100.free(address);
+	} else if (slab_header->size <= 0x200) {
+		return slab_0x200.free(address);
+	} else if (slab_header->size <= 0x500) {
+		return slab_0x300.free(address);
+	} else {
+		assert(false);
+		return;
+	}
 }
--- minimalist_malloc/minimalist_malloc.cpp
+++ minimalist_malloc/minimalist_malloc.cpp
@@ -1,18 +1,55 @@
 #include <iostream>
 #include "slab_allocator.h"
 #include "malloc.h"
+#include <chrono> 
+using namespace std::chrono;
+
+template<typename Func>
+long long calculate_execution_time(Func funcion) {
+	const size_t iterations = 1000;
+	long long duration = 0;
+	for (size_t i = 0; i < iterations; i++) {
+		auto start = high_resolution_clock::now();
+		funcion();
+		auto stop = high_resolution_clock::now();
+		duration += duration_cast<microseconds>(stop - start).count();
+	}
+	return duration / iterations;
+}
+
+void benchmark_allocation_size(size_t allocation_size) {
+
+	auto my_malloc_time = calculate_execution_time([&]() {
+		for (size_t i = 0; i < 10; i++) {
+			void* addrsss = custom_malloc(allocation_size);
+			memset(addrsss, 0xCC, allocation_size);
+			custom_free(addrsss);
+		}
+	});
+
+	auto malloc_time = calculate_execution_time([&]() {
+		for (size_t i = 0; i < 10; i++) {
+			void* addrsss = malloc(allocation_size);
+			memset(addrsss, 0xCC, allocation_size);
+			free(addrsss);
+		}
+	});
+	printf("For size 0x%X\n", allocation_size);
+	std::cout << "my malloc time: " << my_malloc_time << std::endl;
+	std::cout << "real malloc time: " << malloc_time << std::endl;
+	if (my_malloc_time > malloc_time) {
+		printf("my malloc is %f times slower than the real malloc\n", (double)my_malloc_time / malloc_time);
+	} else {
+		printf("my malloc is %.2f times faster than the real malloc\n", (double)malloc_time / my_malloc_time);
+	}
+	printf("------------------------------------------------\n");
+}
 
 int main() {
 	custom_malloc_init();
-	void* addrsss1 = custom_malloc(4);
-	void* addrsss2 = custom_malloc(20);
-	printf("we got address %x \n", addrsss1);
-	printf("we got address %x \n", addrsss2);
-	custom_free(addrsss1);
-	custom_free(addrsss2);
-	addrsss1 = custom_malloc(0x145);
-	addrsss2 = custom_malloc(0x145);
-	printf("we got address %x \n", addrsss1);
-	printf("we got address %x \n", addrsss2);
 
+	for (size_t i = 1; i < 20; i++) {
+		benchmark_allocation_size(0x10 * i);
+	}
 }
+
